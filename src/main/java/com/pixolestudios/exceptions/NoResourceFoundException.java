fbe3dff48879d3026d3e2eb9faf8ae503a98df60
jrickard <joerickard@hotmail.co.uk>
2019-07-09 13:38:52 +0100

    Updated custom exception messages

--- src/main/java/com/pixolestudios/exceptions/NoResourceFoundException.java
+++ src/main/java/com/pixolestudios/exceptions/NoResourceFoundException.java
@@ -3,10 +3,11 @@ package main.java.com.pixolestudios.exceptions;
 public class NoResourceFoundException extends Exception {
     /**
      * Thrown if resource is not found within resource file
-     * @param path path to resource file
+     *
+     * @param path     path to resource file
      * @param resource the resource which could not be found
      */
     public NoResourceFoundException(String path, String resource) {
-        super("No resource \"" + resource + "\" found in resource file " + path);
+        super("No resource \"" + resource + "\" found in resource file \"" + path + "\"");
     }
 }
